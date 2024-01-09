7efa584306db27e9970c2eb300c30dd70d77fc22
sonashar28 <99951682+sonashar28@users.noreply.github.com>
2024-01-09 19:07:28 +0530

    Add files via upload

---
+++ models.py
@@ -0,0 +1,76 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+
+departments=[('Maternity Unit ','Maternity Unit'),
+('Birthing Unit','Birthing Unit'),
+('General Care Nursery (GCN)','General Care Nursery (GCN)'),
+('Special Care Nursery (SCN)','Special Care Nursery (SCN)'),
+('Psychologist','Psychologist')
+]
+class Doctor(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic= models.ImageField(upload_to='profile_pic/DoctorProfilePic/',null=True,blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=True)
+    department= models.CharField(max_length=50,choices=departments,default='Maternity Unit')
+    status=models.BooleanField(default=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+    def __str__(self):
+        return "{} ({})".format(self.user.first_name,self.department)
+
+
+
+class Patient(models.Model):
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
+    profile_pic= models.ImageField(upload_to='profile_pic/PatientProfilePic/',null=True,blank=True)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=False)
+    dateOfPregnancy = models.CharField(max_length=100,null=False)
+    assignedDoctorId = models.PositiveIntegerField(null=True)
+    consultDate=models.DateField(auto_now=True)
+    status=models.BooleanField(default=False)
+    @property
+    def get_name(self):
+        return self.user.first_name+" "+self.user.last_name
+    @property
+    def get_id(self):
+        return self.user.id
+    def __str__(self):
+        return self.user.first_name+" ("+self.dateOfPregnancy+")"
+
+
+class Appointment(models.Model):
+    patientId=models.PositiveIntegerField(null=True)
+    doctorId=models.PositiveIntegerField(null=True)
+    patientName=models.CharField(max_length=40,null=True)
+    doctorName=models.CharField(max_length=40,null=True)
+    appointmentDate=models.DateField(auto_now=True)
+    description=models.TextField(max_length=500)
+    status=models.BooleanField(default=False)
+
+
+
+class PatientDischargeDetails(models.Model):
+    patientId=models.PositiveIntegerField(null=True)
+    patientName=models.CharField(max_length=40)
+    assignedDoctorName=models.CharField(max_length=40)
+    address = models.CharField(max_length=40)
+    mobile = models.CharField(max_length=20,null=True)
+    dateOfPregnancy = models.CharField(max_length=100,null=True)
+
+    consultDate=models.DateField(null=False)
+    releaseDate=models.DateField(null=False)
+
+    medicineCost=models.PositiveIntegerField(null=False)
+    doctorFee=models.PositiveIntegerField(null=False)
+    OtherCharge=models.PositiveIntegerField(null=False)
+    total=models.PositiveIntegerField(null=False)
+
+
